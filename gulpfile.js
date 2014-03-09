1ba9c037a8fc18f188fe5e805cb6c93b38768aa3
williamsbdev <williamsbdev@gmail.com>
2014-03-08 20:17:08 -0600

    Feature: added gulp-ember-handlebars

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,29 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var handlebars = require('gulp-ember-handlebars');
+
+var templates = ['js/templates/*.handlebars']
 
 var files = [
     "js/vendor/jquery/dist/jquery.min.js",
     "js/vendor/handlebars/handlebars.js",
     "js/vendor/ember/ember.js",
     "js/app.js",
-    "js/vendor/jquery-mockjax/jquery.mockjax.js"
+    "js/vendor/jquery-mockjax/jquery.mockjax.js",
+    "js/dist/tmpl.min.js"
 ]
 
-gulp.task('default', function(){
-    return gulp.src(files)
+gulp.task('concat', function(){
+    gulp.src(files)
         .pipe(concat('app.min.js'))
         .pipe(gulp.dest('js/dist'));
 });
+
+gulp.task('templates', function(){
+    gulp.src(templates)
+        .pipe(handlebars({output: 'browser'}))
+        .pipe(concat('tmpl.min.js'))
+        .pipe(gulp.dest('js/dist'));
+});
+
+gulp.task('default', ['templates', 'concat'])
